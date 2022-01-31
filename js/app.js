dde3e5a77d4881c84916331b6ee623dbb46f59a3
Ferhat KAYMAZ <ferhatkaymaz2023@gmail.com>
2022-01-31 20:03:22 +0300

    app.js sablon

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo');
+const filterInput = document.getElementById('filter');
+const todoList = document.getElementById('list-group');
+const clearButton = document.getElementById('clear-todos');
+const firstCardBody = document.querySelectorAll('.card-body')[0];
+const secondCardBody = document.querySelectorAll('.card-body')[1];
+
+
+addEventListeners();
+
+function addEventListeners()
+{
+}
+
+
